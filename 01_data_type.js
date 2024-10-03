455f06c3c15257804c40d32a059bde94dd5e0d26
ragini <raginiraaz112@gmail.com>
2024-10-04 01:01:48 +0530

    Datatypes

---
+++ 01_data_type.js
@@ -0,0 +1,24 @@
+let age = 10;
+let value = "Ragini";
+let name = "Raman";
+let address = "wjolkepf;32odsopd[skerfpeop";
+
+console.log(Number);
+
+console.log(typeof value);
+console.log(typeof age);
+console.log(typeof address);
+console.log(typeof name); // string
+
+
+// dataypes :
+
+//     Number
+//     String
+//     Boolean
+//     null
+//     undefined
+//     Symbol
+//     BigInt
+//     Object
+//     Function
