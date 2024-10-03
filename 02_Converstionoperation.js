251a648f02cf51f18ed999c51bb8bba71227018a
ragini <raginiraaz112@gmail.com>
2024-10-04 01:02:56 +0530

    DataConvertionoperation

---
+++ 02_Converstionoperation.js
@@ -0,0 +1,27 @@
+// let score = false;
+
+
+// console.log(score);
+// console.log(typeof (score));
+
+ 
+// let valueInnumber = Number(score);
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+let isLoggedIn = "Ragini"
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+// console.log(typeof booleanisLoggedIn);
+
+
+// 1 >= true, 0<= false;
+// "" = false; //"Ragini" = true;
+
+let age = 33;
+
+let stringNumber = String(age);
+console.log(stringNumber);
+console.log(typeof stringNumber);
